ca5f4a62d401ac700d90a4e9f959c7acd2d2abcf
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-23 11:40:35 -0500

    Add each user input to the array named dailyAverageTemp.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,43 @@ public class Main {
     public static void main(String[] args) {
 
 
-        double[] dailyAverageTemp = new double[];
+        double[] dailyAverageTemp = new double[7];
 
         Scanner sc = new Scanner(System.in);
 
-        double userInput = sc.nextDouble(); 
+        System.out.println("Enter temperature");
+
+        double userInput = sc.nextDouble();
+        dailyAverageTemp[0] = userInput;
+
+        System.out.println("Enter temperature");
+        double userInput1 = sc.nextDouble();
+        dailyAverageTemp[1] = userInput1;
+
+        System.out.println("Enter temperature");
+        double userInput2 = sc.nextDouble();
+        dailyAverageTemp[2] = userInput2;
+
+        System.out.println("Enter temperature");
+        double userInput3 = sc.nextDouble();
+        dailyAverageTemp[3] = userInput3;
+
+        System.out.println("Enter temperature");
+        double userInput4 = sc.nextDouble();
+        dailyAverageTemp[4] = userInput4;
+
+        System.out.println("Enter temperature");
+        double userInput5 = sc.nextDouble();
+        dailyAverageTemp[5] = userInput5;
+
+        System.out.println("Enter temperature");
+        double userInput6 = sc.nextDouble();
+        dailyAverageTemp[6] = userInput6;
+
+
+
+
+
+
     }
 }
