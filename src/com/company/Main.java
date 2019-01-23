168c6f8fcd66adf616bd21a6e62e7d8a9ad176fd
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-23 10:49:05 -0500

    Create a scanner object to take in user input for daily average temperatures for 7 days.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 
 
         double[] dailyAverageTemp = new double[];
+
+        Scanner sc = new Scanner(System.in);
+
+        double userInput = sc.nextDouble(); 
     }
 }
