de2346ee4d3c10f08fdcd911b98d40ff3cb4f0ac
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-23 14:56:20 -0500

    Create for each loop to convert each temperature to Celsius. Add print line statements to describe temperatures printing to the console.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,35 +9,36 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter temperature");
+        System.out.println("Enter average temperature for Sunday");
 
         double userInput = sc.nextDouble();
         dailyAverageTemp[0] = userInput;
 
-        System.out.println("Enter temperature");
+        System.out.println("Enter average temperature for Monday");
         double userInput1 = sc.nextDouble();
         dailyAverageTemp[1] = userInput1;
 
-        System.out.println("Enter temperature");
+        System.out.println("Enter average temperature for Tuesday");
         double userInput2 = sc.nextDouble();
         dailyAverageTemp[2] = userInput2;
 
-        System.out.println("Enter temperature");
+        System.out.println("Enter average temperature for Wednesday");
         double userInput3 = sc.nextDouble();
         dailyAverageTemp[3] = userInput3;
 
-        System.out.println("Enter temperature");
+        System.out.println("Enter average temperature for Thursday");
         double userInput4 = sc.nextDouble();
         dailyAverageTemp[4] = userInput4;
 
-        System.out.println("Enter temperature");
+        System.out.println("Enter average temperature for Friday");
         double userInput5 = sc.nextDouble();
         dailyAverageTemp[5] = userInput5;
 
-        System.out.println("Enter temperature");
+        System.out.println("Enter average temperature for Saturday");
         double userInput6 = sc.nextDouble();
         dailyAverageTemp[6] = userInput6;
 
+        System.out.println("The average daily temperatures in fahrenheit for this week are:");
         for(int i = 0; i < dailyAverageTemp.length; i++){
             System.out.println(dailyAverageTemp[i]);
         }
@@ -46,7 +47,13 @@ public class Main {
             totalTemps += dailyAverageTemp[i];
         }
         double averageWeeklyTemp = totalTemps / 7;
-            System.out.println(averageWeeklyTemp);
+            System.out.println("The weekly average temperature is " + averageWeeklyTemp + " degrees fahrenheit.");
+
+        System.out.println("This week's average temperatures in celsius are:");
+        for(double temp : dailyAverageTemp){
+            double celsiusTemp = (temp - 32) / 1.8;
+            System.out.println(celsiusTemp);
+        }
     }
 }
 
